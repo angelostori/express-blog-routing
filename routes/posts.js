6482d3455c7fd9519247f0a158ce254f2f66215a
angelostori <angelostori13@gmail.com>
2025-11-25 12:45:31 +0100

    implement CRUD operations for posts in the posts router

--- routes/posts.js
+++ routes/posts.js
@@ -6,4 +6,29 @@ router.get('/', (req, res) => {
     res.send('Show all posts here')
 })
 
+// show
+router.get('/:id', (req, res) => {
+    res.send(`Show the post with id: ${req.params.id}`)
+})
+
+// store
+router.post('/', (req, res) => {
+    res.send('Store a new post here')
+})
+
+// update
+router.put('/:id', (req, res) => {
+    res.send(`Update the post with id: ${req.params.id}`)
+})
+
+// modify (optional)
+router.patch('/:id', (req, res) => {
+    res.send(`Modify the post with id: ${req.params.id}`)
+})
+
+// destroy
+router.delete('/:id', (req, res) => {
+    res.send(`Delete the post with id: ${req.params.id}`)
+})
+
 module.exports = router
