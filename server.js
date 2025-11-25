58af0c6fa4f23e5e9a086aacc7caef531c1c633e
angelostori <angelostori13@gmail.com>
2025-11-25 12:19:00 +0100

    add posts router and update server to use it

--- server.js
+++ server.js
@@ -25,3 +25,19 @@ Bonus
 Provare a restituire la lista dei post dalla rotta index, in formato json
 Provare a restituire un singolo post dalla rotta show, sempre in formato json
 */
+const express = require('express')
+const app = express()
+const PORT = 3000
+const postsRouter = require('./routes/posts')
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+
+})
+
+// server entry point
+app.get('/', (req, res) => {
+    res.send('Welcome to my blog API server')
+})
+
+app.use('/api/posts', postsRouter)
